1c7cf4f52ed14d48837b7c5c63de820b6af0e664
samyslso26-cell <samyslso26@gmail.com>
2026-04-09 08:55:21 -0300

    SPA

--- arquivo.js
+++ arquivo.js
@@ -23,8 +23,3 @@ class administrador extends usuario {
         console.log("Produto " + nomeProduto + " cadastrado com preço R$" + preco + ".");
     }
 }
-
-const usuario1 = new usuario("Samuel", "samuel@example.com", "senha123");
-const admin1 = new administrador("Admin", "admin@example.com", "admin123", "admin");
-usuario1.fazerLogin("samuel@example.com", "senha123");
-admin1.cadastrarProduto("Notebook", 2500);
